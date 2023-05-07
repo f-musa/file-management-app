65fdfdbe54e6bf0f1a17f8dc3c88cecbc792cc7c
Musa Faal <60163481+f-musa@users.noreply.github.com>
2023-05-07 12:52:41 +0200

    Delete Enums directory

--- Enums/AuthAction.java
+++
@@ -1,6 +0,0 @@
-package Enums;
-
-public enum AuthAction {
-    LOGIN,
-    SIGNUP
-}
--- Enums/FileStatus.java
+++
@@ -1,7 +0,0 @@
-package Enums;
-
-public enum FileStatus {
-    CLIENT_IS_AHEAD,
-    SERVER_IS_AHEAD,
-    SAME_VERSION   
-}
